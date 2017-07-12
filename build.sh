2627b2563aa6a8ab0dfd3471a572e44e0f5b9367
leosilvasouza <leonardosilva.souza@yahoo.com.br>
2017-07-12 11:06:52 -0400

    Build.sh fixed tag receiving ID

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 docker build -t="leomontt/HelloWorld" .
 docker login
-docker tag helloworld leomontt/helloworld:lastest
+docker tag c061a301569 leomontt/helloworld:lastest
 docker push leomontt/helloworld
 
 echo -e "Build completed with success\nCheck your Docker Hub account"
