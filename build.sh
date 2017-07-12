9a4aebefad7b74bf30d3cdb6c3f1535b11ff0b7c
leosilvasouza <leonardosilva.souza@yahoo.com.br>
2017-07-12 10:43:30 -0400

    Increased tag before of Push

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-docker build -t leomontt/HelloWorld .
+docker build -t="leomontt/HelloWorld" .
+docker login
+docker tag helloworld leomontt/helloworld:lastest
+docker push leomontt/helloworld
 
 echo -e "Build completed with success\nCheck your Docker Hub account"
